57765007d128df7ca02bdb1cd6fcb4e13ea9ad36
bello22 <hilarylee1022@gmail.com>
2024-12-21 02:23:18 +0800

    {

--- Tetris/Position.cs
+++ Tetris/Position.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//hi
+
 namespace Tetris 
+{ 
     public class Position
     {
         public int Row { get; set; }
